f458ccbafaaa1d322300fe3990386858bdea4389
quirrel <becoolove@outlook.com>
2024-08-06 23:59:57 +0800

    中间表单

--- src/components/MidContainer.vue
+++ src/components/MidContainer.vue
@@ -1,7 +1,19 @@
 <template>
 	<div class="mid-container">
 		<form class="form">
-
+			<div class="vertical_layout">
+				<div class="experience"></div>
+				<div class="horizontal_layout">
+					<div class="single_line_input">
+						<label>用户名</label>
+						<input type="text" />
+					</div>
+					<div class="single_line_input">
+						<label>密码</label>
+						<input type="password" />
+					</div>
+				</div>
+			</div>
 		</form>
 	</div>
 </template>
